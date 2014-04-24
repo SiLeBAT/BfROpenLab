61722bf64c7d5268af9588172871c3af9d8e1ddf
cthoens <christian.thoens@bfr.bund.de>
2014-04-24 14:12:38 +0200

    diff equations

--- de.bund.bfr.math/src/de/bund/bfr/math/Test.java
+++ de.bund.bfr.math/src/de/bund/bfr/math/Test.java
@@ -24,23 +24,26 @@
  ******************************************************************************/
 package de.bund.bfr.math;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.math3.ode.nonstiff.ClassicalRungeKuttaIntegrator;
 import org.nfunk.jep.ParseException;
 
 public class Test {
 
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
 		try {
-			nls();
+			diff();
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 	}
 
+	@SuppressWarnings("unused")
 	private static void nls() throws ParseException {
 		String formula = "a*x^2+b*x+c";
 		List<String> parameters = Arrays.asList("a", "b", "c");
@@ -58,5 +61,36 @@ public class Test {
 		optimizer.optimize(1, 1, true);
 
 		System.out.println(optimizer.getParameterValues());
+	}
+
+	private static void diff() throws ParseException {
+		String formula = "-1/Dref*exp(ln(10)/z*(T0+time*Trate-Tref))*y";
+		Map<String, Double> paramValues = new LinkedHashMap<String, Double>();
+
+		paramValues.put("Dref", 10.0);
+		paramValues.put("Tref", 30.0);
+		paramValues.put("z", 10.0);
+		paramValues.put("T0", 20.0);
+		paramValues.put("Trate", 1.0);
+
+		DiffFunction2 f = new DiffFunction2(formula, paramValues, "y", "time");
+		ClassicalRungeKuttaIntegrator integrator = new ClassicalRungeKuttaIntegrator(
+				0.01);
+		List<Double> values = new ArrayList<Double>();
+		double[] mem = new double[1];
+		double y = 10.0;
+		double t0 = 0.0;
+		double tStep = 1.0;
+
+		for (int i = 0; i < 100; i++) {
+			integrator.integrate(f, t0 + i * tStep, new double[] { y }, t0
+					+ (i + 1) * tStep, mem);
+			values.add(mem[0]);
+			y = mem[0];
+		}
+
+		for (double v : values) {
+			System.out.print(v + ",");
+		}
 	}
 }
