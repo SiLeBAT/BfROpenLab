77d3ef001653946aca47af163566fb73dfd6743c
thoens <christian.thoens@gmail.com>
2014-08-04 10:55:55 +0200

    Layouts updated

--- de.bund.bfr.knime.gis/src/de/bund/bfr/knime/gis/views/canvas/layout/FRLayout.java
+++ de.bund.bfr.knime.gis/src/de/bund/bfr/knime/gis/views/canvas/layout/FRLayout.java
@@ -1,10 +1,26 @@
-/*
- * Copyright (c) 2003, the JUNG Project and the Regents of the University of
- * California All rights reserved.
- *
- * This software is open-source under the BSD license; see either "license.txt"
- * or http://jung.sourceforge.net/license.txt for a description.
- */
+/*******************************************************************************
+ * Copyright (c) 2014 Federal Institute for Risk Assessment (BfR), Germany 
+ * 
+ * Developers and contributors are 
+ * Christian Thoens (BfR)
+ * Armin A. Weiser (BfR)
+ * Matthias Filter (BfR)
+ * Annemarie Kaesbohrer (BfR)
+ * Bernd Appel (BfR)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package de.bund.bfr.knime.gis.views.canvas.layout;
 
 import java.awt.Dimension;
@@ -22,68 +38,32 @@ import edu.uci.ics.jung.algorithms.util.IterativeContext;
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.util.Pair;
 
-/**
- * Implements the Fruchterman-Reingold force-directed algorithm for node layout.
- * 
- * <p>
- * Behavior is determined by the following settable parameters:
- * <ul>
- * <li/>attraction multiplier: how much edges try to keep their vertices
- * together
- * <li/>repulsion multiplier: how much vertices try to push each other apart
- * <li/>maximum iterations: how many iterations this algorithm will use before
- * stopping
- * </ul>
- * Each of the first two defaults to 0.75; the maximum number of iterations
- * defaults to 700.
- * 
- * @see "Fruchterman and Reingold, 'Graph Drawing by Force-directed Placement'"
- * @see "http://i11www.ilkd.uni-karlsruhe.de/teaching/SS_04/visualisierung/papers/fruchterman91graph.pdf"
- * @author Scott White, Yan-Biao Boey, Danyel Fisher
- */
 public class FRLayout<V, E> extends AbstractLayout<V, E> implements
 		IterativeContext {
 
+	private static final double EPSILON = 0.000001;
+	private static final double ATTRACTION_MULTIPLIER = 0.75;
+	private static final double REPULSION_MULTIPLIER = 0.75;
+	private static final int MAX_ITERATIONS = 700;
+
 	private double forceConstant;
-
 	private double temperature;
-
 	private int currentIteration;
-
-	private int mMaxIterations = 700;
-
-	private Map<V, FRVertexData> frVertexData = LazyMap.decorate(
-			new HashMap<V, FRVertexData>(), new Factory<FRVertexData>() {
-				@Override
-				public FRVertexData create() {
-					return new FRVertexData();
-				}
-			});
-
-	private double attraction_multiplier = 0.75;
-
+	private Map<V, FRVertexData> frVertexData;
 	private double attraction_constant;
-
-	private double repulsion_multiplier = 0.75;
-
 	private double repulsion_constant;
-
 	private double max_dimension;
 
-	/**
-	 * Creates an instance for the specified graph.
-	 */
 	public FRLayout(Graph<V, E> g) {
 		super(g);
-	}
 
-	/**
-	 * Creates an instance of size {@code d} for the specified graph.
-	 */
-	public FRLayout(Graph<V, E> g, Dimension d) {
-		super(g, new RandomLocationTransformer<V>(d), d);
-		initialize();
-		max_dimension = Math.max(d.height, d.width);
+		frVertexData = LazyMap.decorate(new HashMap<V, FRVertexData>(),
+				new Factory<FRVertexData>() {
+					@Override
+					public FRVertexData create() {
+						return new FRVertexData();
+					}
+				});
 	}
 
 	@Override
@@ -91,50 +71,47 @@ public class FRLayout<V, E> extends AbstractLayout<V, E> implements
 		if (initialized == false) {
 			setInitializer(new RandomLocationTransformer<V>(size));
 		}
+
 		super.setSize(size);
 		max_dimension = Math.max(size.height, size.width);
 	}
 
-	/**
-	 * Sets the attraction multiplier.
-	 */
-	public void setAttractionMultiplier(double attraction) {
-		this.attraction_multiplier = attraction;
-	}
-
-	/**
-	 * Sets the repulsion multiplier.
-	 */
-	public void setRepulsionMultiplier(double repulsion) {
-		this.repulsion_multiplier = repulsion;
-	}
-
 	@Override
 	public void reset() {
-		doInit();
+		initialize();
 	}
 
 	@Override
 	public void initialize() {
-		doInit();
-	}
-
-	private void doInit() {
 		Graph<V, E> graph = getGraph();
 		Dimension d = getSize();
-		if (graph != null && d != null) {
-			currentIteration = 0;
-			temperature = d.getWidth() / 10;
 
-			forceConstant = Math.sqrt(d.getHeight() * d.getWidth()
-					/ graph.getVertexCount());
-
-			attraction_constant = attraction_multiplier * forceConstant;
-			repulsion_constant = repulsion_multiplier * forceConstant;
+		if (graph == null || d == null) {
+			return;
 		}
+
+		currentIteration = 0;
+		temperature = d.getWidth() / 10;
+
+		forceConstant = Math.sqrt(d.getHeight() * d.getWidth()
+				/ graph.getVertexCount());
+
+		attraction_constant = ATTRACTION_MULTIPLIER * forceConstant;
+		repulsion_constant = REPULSION_MULTIPLIER * forceConstant;
 	}
 
-	private double EPSILON = 0.000001D;
+	/**
+	 * Returns true once the current iteration has passed the maximum count,
+	 * <tt>MAX_ITERATIONS</tt>.
+	 */
+	@Override
+	public boolean done() {
+		if (currentIteration > MAX_ITERATIONS
+				|| temperature < 1.0 / max_dimension) {
+			return true;
+		}
+		return false;
+	}
 
 	/**
 	 * Moves the iteration forward one notch, calculation attraction and
@@ -153,6 +130,7 @@ public class FRLayout<V, E> extends AbstractLayout<V, E> implements
 				for (V v1 : getGraph().getVertices()) {
 					calcRepulsion(v1);
 				}
+
 				break;
 			} catch (ConcurrentModificationException cme) {
 			}
@@ -164,9 +142,9 @@ public class FRLayout<V, E> extends AbstractLayout<V, E> implements
 		while (true) {
 			try {
 				for (E e : getGraph().getEdges()) {
-
 					calcAttraction(e);
 				}
+
 				break;
 			} catch (ConcurrentModificationException cme) {
 			}
@@ -175,24 +153,27 @@ public class FRLayout<V, E> extends AbstractLayout<V, E> implements
 		while (true) {
 			try {
 				for (V v : getGraph().getVertices()) {
-					if (isLocked(v))
-						continue;
-					calcPositions(v);
+					if (!isLocked(v)) {
+						calcPositions(v);
+					}
 				}
 				break;
 			} catch (ConcurrentModificationException cme) {
 			}
 		}
+
 		cool();
 	}
 
-	protected synchronized void calcPositions(V v) {
-		FRVertexData fvd = getFRData(v);
-		if (fvd == null)
+	private synchronized void calcPositions(V v) {
+		FRVertexData fvd = frVertexData.get(v);
+
+		if (fvd == null) {
 			return;
+		}
+
 		Point2D xyd = transform(v);
 		double deltaLength = Math.max(EPSILON, fvd.norm());
-
 		double newXDisp = fvd.getX() / deltaLength
 				* Math.min(deltaLength, temperature);
 
@@ -203,10 +184,11 @@ public class FRLayout<V, E> extends AbstractLayout<V, E> implements
 
 		double newYDisp = fvd.getY() / deltaLength
 				* Math.min(deltaLength, temperature);
+
 		xyd.setLocation(xyd.getX() + newXDisp, xyd.getY() + newYDisp);
 	}
 
-	protected void calcAttraction(E e) {
+	private void calcAttraction(E e) {
 		Pair<V> endpoints = getGraph().getEndpoints(e);
 		V v1 = endpoints.getFirst();
 		V v2 = endpoints.getSecond();
@@ -217,16 +199,18 @@ public class FRLayout<V, E> extends AbstractLayout<V, E> implements
 			// both locked, do nothing
 			return;
 		}
+
 		Point2D p1 = transform(v1);
 		Point2D p2 = transform(v2);
-		if (p1 == null || p2 == null)
+
+		if (p1 == null || p2 == null) {
 			return;
+		}
+
 		double xDelta = p1.getX() - p2.getX();
 		double yDelta = p1.getY() - p2.getY();
-
 		double deltaLength = Math.max(EPSILON,
 				Math.sqrt((xDelta * xDelta) + (yDelta * yDelta)));
-
 		double force = (deltaLength * deltaLength) / attraction_constant;
 
 		if (Double.isNaN(force)) {
@@ -236,48 +220,53 @@ public class FRLayout<V, E> extends AbstractLayout<V, E> implements
 
 		double dx = (xDelta / deltaLength) * force;
 		double dy = (yDelta / deltaLength) * force;
+
 		if (v1_locked == false) {
-			FRVertexData fvd1 = getFRData(v1);
+			FRVertexData fvd1 = frVertexData.get(v1);
 			fvd1.offset(-dx, -dy);
 		}
+
 		if (v2_locked == false) {
-			FRVertexData fvd2 = getFRData(v2);
+			FRVertexData fvd2 = frVertexData.get(v2);
 			fvd2.offset(dx, dy);
 		}
 	}
 
-	protected void calcRepulsion(V v1) {
-		FRVertexData fvd1 = getFRData(v1);
-		if (fvd1 == null)
+	private void calcRepulsion(V v1) {
+		FRVertexData fvd1 = frVertexData.get(v1);
+
+		if (fvd1 == null) {
 			return;
+		}
+
 		fvd1.setLocation(0, 0);
 
 		try {
 			for (V v2 : getGraph().getVertices()) {
-
-				// if (isLocked(v2)) continue;
-				if (v1 != v2) {
-					Point2D p1 = transform(v1);
-					Point2D p2 = transform(v2);
-					if (p1 == null || p2 == null)
-						continue;
-					double xDelta = p1.getX() - p2.getX();
-					double yDelta = p1.getY() - p2.getY();
-
-					double deltaLength = Math.max(EPSILON,
-							Math.sqrt((xDelta * xDelta) + (yDelta * yDelta)));
-
-					double force = (repulsion_constant * repulsion_constant)
-							/ deltaLength;
-
-					if (Double.isNaN(force)) {
-						throw new RuntimeException(
-								"Unexpected mathematical result in FRLayout:calcPositions [repulsion]");
-					}
-
-					fvd1.offset((xDelta / deltaLength) * force,
-							(yDelta / deltaLength) * force);
+				if (v1 == v2) {
+					continue;
 				}
+
+				Point2D p1 = transform(v1);
+				Point2D p2 = transform(v2);
+				if (p1 == null || p2 == null)
+					continue;
+				double xDelta = p1.getX() - p2.getX();
+				double yDelta = p1.getY() - p2.getY();
+
+				double deltaLength = Math.max(EPSILON,
+						Math.sqrt((xDelta * xDelta) + (yDelta * yDelta)));
+
+				double force = (repulsion_constant * repulsion_constant)
+						/ deltaLength;
+
+				if (Double.isNaN(force)) {
+					throw new RuntimeException(
+							"Unexpected mathematical result in FRLayout:calcPositions [repulsion]");
+				}
+
+				fvd1.offset((xDelta / deltaLength) * force,
+						(yDelta / deltaLength) * force);
 			}
 		} catch (ConcurrentModificationException cme) {
 			calcRepulsion(v1);
@@ -285,41 +274,10 @@ public class FRLayout<V, E> extends AbstractLayout<V, E> implements
 	}
 
 	private void cool() {
-		temperature *= (1.0 - currentIteration / (double) mMaxIterations);
+		temperature *= (1.0 - currentIteration / (double) MAX_ITERATIONS);
 	}
 
-	/**
-	 * Sets the maximum number of iterations.
-	 */
-	public void setMaxIterations(int maxIterations) {
-		mMaxIterations = maxIterations;
-	}
-
-	protected FRVertexData getFRData(V v) {
-		return frVertexData.get(v);
-	}
-
-	/**
-	 * This one is an incremental visualization.
-	 */
-	public boolean isIncremental() {
-		return true;
-	}
-
-	/**
-	 * Returns true once the current iteration has passed the maximum count,
-	 * <tt>MAX_ITERATIONS</tt>.
-	 */
-	@Override
-	public boolean done() {
-		if (currentIteration > mMaxIterations
-				|| temperature < 1.0 / max_dimension) {
-			return true;
-		}
-		return false;
-	}
-
-	protected static class FRVertexData extends Point2D.Double {
+	private static class FRVertexData extends Point2D.Double {
 
 		private static final long serialVersionUID = 1L;
 
