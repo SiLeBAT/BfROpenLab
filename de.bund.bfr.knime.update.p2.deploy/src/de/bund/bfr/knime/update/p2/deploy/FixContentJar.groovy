15f59f4dfab10638a94b0334da36148f72e09e9b
thoens <thoens@192.168.224.63>
2015-02-19 11:20:09 +0100

    tu

--- de.bund.bfr.knime.update.p2.deploy/src/de/bund/bfr/knime/update/p2/deploy/FixContentJar.groovy
+++ de.bund.bfr.knime.update.p2.deploy/src/de/bund/bfr/knime/update/p2/deploy/FixContentJar.groovy
@@ -26,8 +26,9 @@ import java.util.zip.ZipOutputStream
 class FixContentJar {
 
 	static String CATEGORY = "bfropenlab"
+	//	static String CATEGORY = "other"
 	static String UPDATE_SITE = "../de.bund.bfr.knime.update.p2"
-	//static String UPDATE_SITE = "../../Other/de.bund.bfr.knime.flink.update.p2"
+	//	static String UPDATE_SITE = "../../Other/de.bund.bfr.knime.flink.update.p2"
 
 	static String CONTENT_JAR = "content.jar"
 	static String CONTENT_XML = "content.xml"
