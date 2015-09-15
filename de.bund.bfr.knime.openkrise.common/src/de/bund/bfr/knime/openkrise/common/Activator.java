f0497b90787cba440a0f8933b5a44a968d4af2aa
thoens <christian.thoens@gmail.com>
2015-09-15 12:18:51 +0200

    de.bund.bfr.knime.openkrise.common created

---
+++ de.bund.bfr.knime.openkrise.common/src/de/bund/bfr/knime/openkrise/common/Activator.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Federal Institute for Risk Assessment (BfR), Germany
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *     Department Biological Safety - BfR
+ *******************************************************************************/
+package de.bund.bfr.knime.openkrise.common;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * This is the eclipse bundle activator. Note: KNIME node developers probably
+ * won't have to do anything in here, as this class is only needed by the
+ * eclipse platform/plugin mechanism. If you want to move/rename this file, make
+ * sure to change the plugin.xml file in the project root directory accordingly.
+ * 
+ * @author Christian Thoens
+ */
+public class Activator extends Plugin {
+
+	/**
+	 * This method is called upon plug-in activation.
+	 * 
+	 * @param context
+	 *            The OSGI bundle context
+	 * @throws Exception
+	 *             If this plugin could not be started
+	 */
+	@Override
+	public void start(final BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	/**
+	 * This method is called when the plug-in is stopped.
+	 * 
+	 * @param context
+	 *            The OSGI bundle context
+	 * @throws Exception
+	 *             If this plugin could not be stopped
+	 */
+	@Override
+	public void stop(final BundleContext context) throws Exception {
+		super.stop(context);
+	}
+}
