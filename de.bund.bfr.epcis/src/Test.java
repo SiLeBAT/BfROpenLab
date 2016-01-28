651ec43ba7fc80253f8163d7379c9df0470b01d0
thoens <christian.thoens@gmail.com>
2016-01-28 17:05:21 +0100

    tu

--- de.bund.bfr.epcis/src/Test.java
+++ de.bund.bfr.epcis/src/Test.java
@@ -2,16 +2,44 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 
+import de.bund.bfr.epcis.EPCISBodyType;
 import de.bund.bfr.epcis.EPCISDocumentType;
+import de.bund.bfr.epcis.EPCISHeaderType;
 import de.bund.bfr.epcis.ObjectFactory;
+import de.bund.bfr.epcis.Partner;
+import de.bund.bfr.epcis.PartnerIdentification;
+import de.bund.bfr.epcis.StandardBusinessDocumentHeader;
 
 public class Test {
 
 	private static final ObjectFactory FACTORY = new ObjectFactory();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
+		// HEADER
+		
+		Partner bauernhof = createPartner("Bauernhof");
+		Partner molkerei = createPartner("Molkerei");
+		StandardBusinessDocumentHeader standardHeader = FACTORY.createStandardBusinessDocumentHeader();
+		
+		standardHeader.setHeaderVersion("First Version");
+		standardHeader.getSender().add(bauernhof);
+		standardHeader.getReceiver().add(molkerei);
+		
+		EPCISHeaderType header = FACTORY.createEPCISHeaderType();
+		
+		header.setStandardBusinessDocumentHeader(standardHeader);
+		
+		// BODY		
+		
+		EPCISBodyType body = FACTORY.createEPCISBodyType();		
+		
+		// DOCUMENT
+		
 		EPCISDocumentType doc = FACTORY.createEPCISDocumentType();
-
+		
+		doc.setEPCISHeader(header);
+		doc.setEPCISBody(body);
+		
 		try {
 			Marshaller jaxbMarshaller = JAXBContext.newInstance(ObjectFactory.class.getPackage().getName())
 					.createMarshaller();
@@ -22,5 +50,17 @@ public class Test {
 		} catch (JAXBException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private static Partner createPartner(String id) {
+		PartnerIdentification ident = FACTORY.createPartnerIdentification();
+				
+		ident.setValue(id);
+		
+		Partner p = FACTORY.createPartner();
+		
+		p.setIdentifier(ident);
+		
+		return p;
 	}
 }
