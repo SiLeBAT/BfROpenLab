f637273cdb10c84dfe271c32e3b7a3944253d4b2
draaw <armin.weiser@bfr.bund.de>
2016-03-30 11:59:43 +0200

    tu

--- de.bund.bfr.busstop/src/de/bund/bfr/busstop/Constants.java
+++ de.bund.bfr.busstop/src/de/bund/bfr/busstop/Constants.java
@@ -1,7 +1,9 @@
 package de.bund.bfr.busstop;
 
 public class Constants {
-	public static final String SERVER_UPLOAD_LOCATION_FOLDER = "C:/Users/Armin/Desktop/busstop_folder/";
+	//public static final String SERVER_UPLOAD_LOCATION_FOLDER = "C:/Users/Armin/Desktop/busstop_folder/";
+	//public static final String SERVER_UPLOAD_LOCATION_FOLDER = "C:/Users/weiser/Desktop/busstop_folder/";
+	public static final String SERVER_UPLOAD_LOCATION_FOLDER = "/home/knime/busstop_folder";
 	public static final String COMMENT_FILENAME = "comment.txt";
 	public static final String DELETED_FILENAME = "deleted.txt";
 }
