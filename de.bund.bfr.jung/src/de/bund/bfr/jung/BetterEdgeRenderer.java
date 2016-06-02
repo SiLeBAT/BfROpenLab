ab5a609eebcaac5f4ba742252790c4e49be143cd
thoens <christian.thoens@bfr.bund.de>
2016-06-02 15:07:40 +0200

    Painting of selected edges improved

--- de.bund.bfr.jung/src/de/bund/bfr/jung/BetterEdgeRenderer.java
+++ de.bund.bfr.jung/src/de/bund/bfr/jung/BetterEdgeRenderer.java
@@ -41,87 +41,83 @@ public class BetterEdgeRenderer<V, E> extends BasicEdgeRenderer<V, E> {
 
 	@Override
 	@SuppressWarnings("unchecked")
-	protected void drawSimpleEdge(RenderContext<V, E> rc, Layout<V, E> layout, E e) {
+	public void paintEdge(RenderContext<V, E> rc, Layout<V, E> layout, E e) {
 		GraphicsDecorator g = rc.getGraphicsContext();
 		Graph<V, E> graph = layout.getGraph();
+
+		if (!rc.getEdgeIncludePredicate().evaluate(Context.<Graph<V, E>, E> getInstance(graph, e))) {
+			return;
+		}
+
 		Pair<V> endpoints = graph.getEndpoints(e);
-		Point2D p1 = layout.transform(endpoints.getFirst());
-		Point2D p2 = layout.transform(endpoints.getSecond());
-		p1 = rc.getMultiLayerTransformer().transform(Layer.LAYOUT, p1);
-		p2 = rc.getMultiLayerTransformer().transform(Layer.LAYOUT, p2);
-		float x1 = (float) p1.getX();
-		float y1 = (float) p1.getY();
-		float x2 = (float) p2.getX();
-		float y2 = (float) p2.getY();
+
+		if (!rc.getVertexIncludePredicate().evaluate(Context.<Graph<V, E>, V> getInstance(graph, endpoints.getFirst()))
+				|| !rc.getVertexIncludePredicate()
+						.evaluate(Context.<Graph<V, E>, V> getInstance(graph, endpoints.getSecond()))) {
+			return;
+		}
+
+		Point2D p1 = rc.getMultiLayerTransformer().transform(Layer.LAYOUT, layout.transform(endpoints.getFirst()));
+		Point2D p2 = rc.getMultiLayerTransformer().transform(Layer.LAYOUT, layout.transform(endpoints.getSecond()));
 		Shape edgeShape = rc.getEdgeShapeTransformer().transform(Context.<Graph<V, E>, E> getInstance(graph, e));
 		Rectangle deviceRectangle = new Rectangle(rc.getScreenDevice().getSize());
-		AffineTransform xform = AffineTransform.getTranslateInstance(x1, y1);
+		AffineTransform edgeTransform = AffineTransform.getTranslateInstance(p1.getX(), p1.getY());
 
 		if (endpoints.getFirst().equals(endpoints.getSecond())) {
 			Rectangle2D bounds = rc.getVertexShapeTransformer().transform(endpoints.getFirst()).getBounds2D();
 
-			xform.scale(bounds.getWidth(), bounds.getHeight());
-			xform.translate(0, -edgeShape.getBounds2D().getWidth() / 2);
+			edgeTransform.scale(bounds.getWidth(), bounds.getHeight());
+			edgeTransform.translate(0, -edgeShape.getBounds2D().getWidth() / 2);
 		} else {
-			float dx = x2 - x1;
-			float dy = y2 - y1;
+			double dx = p2.getX() - p1.getX();
+			double dy = p2.getY() - p1.getY();
 
-			xform.rotate(Math.atan2(dy, dx));
-			xform.scale(Math.sqrt(dx * dx + dy * dy), 1.0);
+			edgeTransform.rotate(Math.atan2(dy, dx));
+			edgeTransform.scale(Math.sqrt(dx * dx + dy * dy), 1.0);
 		}
 
-		edgeShape = xform.createTransformedShape(edgeShape);
+		edgeShape = edgeTransform.createTransformedShape(edgeShape);
 
 		if (rc.getMultiLayerTransformer().getTransformer(Layer.VIEW).transform(edgeShape).intersects(deviceRectangle)) {
 			Paint oldPaint = g.getPaint();
-			BasicStroke stroke = (BasicStroke) rc.getEdgeStrokeTransformer().transform(e);
-			Paint draw_paint = rc.getEdgeDrawPaintTransformer().transform(e);
-			Paint fill_paint = rc.getEdgeFillPaintTransformer().transform(e);
+			Stroke oldStroke = g.getStroke();
 
-			if (draw_paint != null) {
+			BasicStroke stroke = (BasicStroke) rc.getEdgeStrokeTransformer().transform(e);
+			Paint drawPaint = rc.getEdgeDrawPaintTransformer().transform(e);
+
+			if (drawPaint != null) {
 				g.setStroke(new BasicStroke(stroke.getLineWidth() + 4, stroke.getEndCap(), stroke.getLineJoin(),
 						stroke.getMiterLimit(), stroke.getDashArray(), stroke.getDashPhase()));
-				g.setPaint(draw_paint);
+				g.setPaint(drawPaint);
 				g.draw(edgeShape);
 			}
 
-			if (fill_paint != null) {
-				g.setStroke(stroke);
-				g.setPaint(fill_paint);
-				g.draw(edgeShape);
-			}
+			g.setStroke(stroke);
+			g.setPaint(rc.getEdgeFillPaintTransformer().transform(e));
+			g.draw(edgeShape);
 
 			if (rc.getEdgeArrowPredicate().evaluate(Context.<Graph<V, E>, E> getInstance(graph, e))) {
-				Stroke new_stroke = rc.getEdgeArrowStrokeTransformer().transform(e);
-				Stroke old_stroke = g.getStroke();
-
-				if (new_stroke != null) {
-					g.setStroke(new_stroke);
-				}
-
-				Shape destVertexShape = rc.getVertexShapeTransformer().transform(graph.getEndpoints(e).getSecond());
-				AffineTransform xf = AffineTransform.getTranslateInstance(x2, y2);
-				destVertexShape = xf.createTransformedShape(destVertexShape);
+				AffineTransform destVertexTransform = AffineTransform.getTranslateInstance(p2.getX(), p2.getY());
+				Shape destVertexShape = destVertexTransform.createTransformedShape(
+						rc.getVertexShapeTransformer().transform(graph.getEndpoints(e).getSecond()));
 
 				if (rc.getMultiLayerTransformer().getTransformer(Layer.VIEW).transform(destVertexShape)
 						.intersects(deviceRectangle)) {
-					AffineTransform at = edgeArrowRenderingSupport.getArrowTransform(rc, edgeShape, destVertexShape);
-					Shape arrow = rc.getEdgeArrowTransformer()
-							.transform(Context.<Graph<V, E>, E> getInstance(graph, e));
+					AffineTransform arrowTransform = edgeArrowRenderingSupport.getArrowTransform(rc, edgeShape,
+							destVertexShape);
+					Shape arrow = arrowTransform.createTransformedShape(
+							rc.getEdgeArrowTransformer().transform(Context.<Graph<V, E>, E> getInstance(graph, e)));
 
-					arrow = at.createTransformedShape(arrow);
+					g.setStroke(rc.getEdgeArrowStrokeTransformer().transform(e));
 					g.setPaint(rc.getArrowFillPaintTransformer().transform(e));
 					g.fill(arrow);
 					g.setPaint(rc.getArrowDrawPaintTransformer().transform(e));
 					g.draw(arrow);
 				}
-
-				if (new_stroke != null) {
-					g.setStroke(old_stroke);
-				}
 			}
 
 			g.setPaint(oldPaint);
+			g.setStroke(oldStroke);
 		}
 	}
 }
