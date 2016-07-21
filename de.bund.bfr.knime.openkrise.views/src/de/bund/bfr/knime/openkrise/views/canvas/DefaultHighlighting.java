f7bfe108462529325c14d328c0ddb55604bd9fdf
thoens <christian.thoens@bfr.bund.de>
2016-07-21 14:17:11 +0200

    copyrights updated

--- de.bund.bfr.knime.openkrise.views/src/de/bund/bfr/knime/openkrise/views/canvas/DefaultHighlighting.java
+++ de.bund.bfr.knime.openkrise.views/src/de/bund/bfr/knime/openkrise/views/canvas/DefaultHighlighting.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 Federal Institute for Risk Assessment (BfR), Germany
+ * Copyright (c) 2016 German Federal Institute for Risk Assessment (BfR)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
