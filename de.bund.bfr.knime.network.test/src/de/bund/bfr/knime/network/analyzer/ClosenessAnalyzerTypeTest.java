f96791b89500d916ddf1fa1961457e7485481716
thoens <christian.thoens@bfr.bund.de>
2017-01-27 13:21:57 +0100

    initial commit of unit tests for "Network Utils" (not implemented yet)

---
+++ de.bund.bfr.knime.network.test/src/de/bund/bfr/knime/network/analyzer/ClosenessAnalyzerTypeTest.java
@@ -0,0 +1,33 @@
+package de.bund.bfr.knime.network.analyzer;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ClosenessAnalyzerTypeTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testNumericAnalyzeInternalExecutionMonitorKPartiteGraphViewOfPersistentObjectPartitionPersistentObject() {
+		fail("Not yet implemented");
+	}
+}
