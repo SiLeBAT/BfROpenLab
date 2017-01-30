55ae90e7b9b400feeacfc122e862e2532788fdbe
thoens <christian.thoens@bfr.bund.de>
2017-01-30 17:01:51 +0100

    unit tests for Network Utils updated

--- de.bund.bfr.knime.network.test/src/de/bund/bfr/knime/network/analyzer/ClosenessAnalyzerTypeTest.java
+++ de.bund.bfr.knime.network.test/src/de/bund/bfr/knime/network/analyzer/ClosenessAnalyzerTypeTest.java
@@ -24,8 +24,9 @@ import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
 import java.util.Iterator;
 
-import org.junit.Rule;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.knime.core.node.CanceledExecutionException;
 import org.knime.core.node.ExecutionMonitor;
 import org.knime.network.core.api.GraphObjectIterator;
@@ -35,31 +36,40 @@ import org.knime.network.core.api.PersistentObject;
 import org.knime.network.core.core.exception.PersistenceException;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ClosenessAnalyzerTypeTest {
 
 	@Mock
 	KPartiteGraphView<PersistentObject, Partition> view;
 
-	@Rule
-	public MockitoRule mockitoRule = MockitoJUnit.rule();
+	private PersistentObject n1;
+	private PersistentObject n2;
+	private PersistentObject n3;
+	private PersistentObject n4;
+	private PersistentObject n5;
+	private PersistentObject e12;
+	private PersistentObject e13;
+	private PersistentObject e14;
+	private PersistentObject e15;
+	private PersistentObject e23;
+	private PersistentObject e34;
 
-	@Test
-	public void testNumericAnalyzeInternalExecutionMonitorKPartiteGraphViewOfPersistentObjectPartitionPersistentObject()
-			throws PersistenceException, CanceledExecutionException {
-		PersistentObject n1 = () -> "n1";
-		PersistentObject n2 = () -> "n2";
-		PersistentObject n3 = () -> "n3";
-		PersistentObject n4 = () -> "n4";
-		PersistentObject n5 = () -> "n5";
-		PersistentObject e12 = () -> "e12";
-		PersistentObject e13 = () -> "e13";
-		PersistentObject e14 = () -> "e14";
-		PersistentObject e15 = () -> "e15";
-		PersistentObject e23 = () -> "e23";
-		PersistentObject e34 = () -> "e34";
+	@Before
+	public void setUp() throws PersistenceException {
+		n1 = () -> "n1";
+		n2 = () -> "n2";
+		n3 = () -> "n3";
+		n4 = () -> "n4";
+		n5 = () -> "n5";
+		e12 = () -> "e12";
+		e13 = () -> "e13";
+		e14 = () -> "e14";
+		e15 = () -> "e15";
+		e23 = () -> "e23";
+		e34 = () -> "e34";
+
 		PersistentObject[] nodes = { n1, n2, n3, n4, n5 };
 		PersistentObject[] edges = { e12, e13, e14, e15, e23, e34 };
 
@@ -67,23 +77,29 @@ public class ClosenessAnalyzerTypeTest {
 		Mockito.when(view.getNoOfEdges()).thenReturn((long) edges.length);
 		Mockito.when(view.getNodes()).thenReturn(asIterator(nodes));
 		Mockito.when(view.getEdges()).thenReturn(asIterator(edges));
-		Mockito.when(view.getEdgeWeight(e12)).thenReturn(1.0);
-		Mockito.when(view.getEdgeWeight(e13)).thenReturn(1.0);
-		Mockito.when(view.getEdgeWeight(e14)).thenReturn(1.0);
-		Mockito.when(view.getEdgeWeight(e15)).thenReturn(1.0);
-		Mockito.when(view.getEdgeWeight(e23)).thenReturn(1.0);
-		Mockito.when(view.getEdgeWeight(e34)).thenReturn(1.0);
+
 		Mockito.when(view.getIncidentNodes(e12)).thenReturn(Arrays.asList(n1, n2));
 		Mockito.when(view.getIncidentNodes(e13)).thenReturn(Arrays.asList(n1, n3));
 		Mockito.when(view.getIncidentNodes(e14)).thenReturn(Arrays.asList(n1, n4));
 		Mockito.when(view.getIncidentNodes(e15)).thenReturn(Arrays.asList(n1, n5));
 		Mockito.when(view.getIncidentNodes(e23)).thenReturn(Arrays.asList(n2, n3));
 		Mockito.when(view.getIncidentNodes(e34)).thenReturn(Arrays.asList(n3, n4));
+
 		Mockito.when(view.getOutgoingEdges(n1)).thenReturn(Arrays.asList(e12, e13, e14, e15));
 		Mockito.when(view.getOutgoingEdges(n2)).thenReturn(Arrays.asList(e12, e23));
 		Mockito.when(view.getOutgoingEdges(n3)).thenReturn(Arrays.asList(e13, e23, e34));
 		Mockito.when(view.getOutgoingEdges(n4)).thenReturn(Arrays.asList(e14, e34));
 		Mockito.when(view.getOutgoingEdges(n5)).thenReturn(Arrays.asList(e15));
+	}
+
+	@Test
+	public void testWithoutWeights() throws PersistenceException, CanceledExecutionException {
+		Mockito.when(view.getEdgeWeight(e12)).thenReturn(1.0);
+		Mockito.when(view.getEdgeWeight(e13)).thenReturn(1.0);
+		Mockito.when(view.getEdgeWeight(e14)).thenReturn(1.0);
+		Mockito.when(view.getEdgeWeight(e15)).thenReturn(1.0);
+		Mockito.when(view.getEdgeWeight(e23)).thenReturn(1.0);
+		Mockito.when(view.getEdgeWeight(e34)).thenReturn(1.0);
 
 		ClosenessAnalyzerType analyzer = new ClosenessAnalyzerType();
 
@@ -94,6 +110,26 @@ public class ClosenessAnalyzerTypeTest {
 		assertEquals(1.0 / 5.0, analyzer.numericAnalyzeInternal(new ExecutionMonitor(), view, n3)[0], 0.0);
 		assertEquals(1.0 / 6.0, analyzer.numericAnalyzeInternal(new ExecutionMonitor(), view, n4)[0], 0.0);
 		assertEquals(1.0 / 7.0, analyzer.numericAnalyzeInternal(new ExecutionMonitor(), view, n5)[0], 0.0);
+	}
+
+	@Test
+	public void testWithWeights() throws PersistenceException, CanceledExecutionException {
+		Mockito.when(view.getEdgeWeight(e12)).thenReturn(2.0);
+		Mockito.when(view.getEdgeWeight(e13)).thenReturn(2.0);
+		Mockito.when(view.getEdgeWeight(e14)).thenReturn(2.0);
+		Mockito.when(view.getEdgeWeight(e15)).thenReturn(2.0);
+		Mockito.when(view.getEdgeWeight(e23)).thenReturn(1.0);
+		Mockito.when(view.getEdgeWeight(e34)).thenReturn(1.0);
+
+		ClosenessAnalyzerType analyzer = new ClosenessAnalyzerType();
+
+		analyzer.initializeInternal(view, new ExecutionMonitor());
+
+		assertEquals(1.0 / 8.0, analyzer.numericAnalyzeInternal(new ExecutionMonitor(), view, n1)[0], 0.0);
+		assertEquals(1.0 / 9.0, analyzer.numericAnalyzeInternal(new ExecutionMonitor(), view, n2)[0], 0.0);
+		assertEquals(1.0 / 8.0, analyzer.numericAnalyzeInternal(new ExecutionMonitor(), view, n3)[0], 0.0);
+		assertEquals(1.0 / 9.0, analyzer.numericAnalyzeInternal(new ExecutionMonitor(), view, n4)[0], 0.0);
+		assertEquals(1.0 / 14.0, analyzer.numericAnalyzeInternal(new ExecutionMonitor(), view, n5)[0], 0.0);
 	}
 
 	private static GraphObjectIterator<PersistentObject> asIterator(PersistentObject... objects) {
