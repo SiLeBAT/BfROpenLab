be4531c7e70ea2eabeb203455cc8d2b0a9f93312
thoens <christian.thoens@bfr.bund.de>
2017-02-03 13:01:18 +0100

    first Tracing test implemented

--- de.bund.bfr.knime.openkrise.test/src/de/bund/bfr/knime/openkrise/TracingTest.java
+++ de.bund.bfr.knime.openkrise.test/src/de/bund/bfr/knime/openkrise/TracingTest.java
@@ -19,24 +19,88 @@
  *******************************************************************************/
 package de.bund.bfr.knime.openkrise;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import com.google.common.collect.ImmutableSet;
+
+import de.bund.bfr.knime.openkrise.common.Delivery;
+
 public class TracingTest {
+
+	private static final String FARM_1 = "farm1";
+	private static final String FARM_2 = "farm2";
+	private static final String FARM_3 = "farm3";
+
+	private static final String PRODUCER_1 = "producer1";
+	private static final String PRODUCER_2 = "producer2";
+
+	private static final String MARKET_1 = "market1";
+	private static final String MARKET_2 = "market2";
+	private static final String MARKET_3 = "market3";
+
+	private static final String F1P1_1 = FARM_1 + PRODUCER_1 + 1;
+	private static final String F1P1_2 = FARM_1 + PRODUCER_1 + 2;
+	private static final String F2P1_1 = FARM_2 + PRODUCER_1 + 1;
+	private static final String F2P2_1 = FARM_2 + PRODUCER_2 + 1;
+	private static final String F3P2_1 = FARM_3 + PRODUCER_2 + 1;
+
+	private static final String P1M1_1 = PRODUCER_1 + MARKET_1 + 1;
+	private static final String P1M2_1 = PRODUCER_1 + MARKET_2 + 1;
+	private static final String P2M2_1 = PRODUCER_2 + MARKET_2 + 1;
+	private static final String P2M3_1 = PRODUCER_2 + MARKET_3 + 1;
+
+	private List<Delivery> deliveries;
 
 	@Before
 	public void setUp() {
+		deliveries = new ArrayList<>();
+
+		deliveries.add(new Delivery.Builder(F1P1_1, FARM_1, PRODUCER_1)
+				.connectedDeliveries(ImmutableSet.of(), ImmutableSet.of(P1M1_1)).build());
+		deliveries.add(new Delivery.Builder(F1P1_2, FARM_1, PRODUCER_1)
+				.connectedDeliveries(ImmutableSet.of(), ImmutableSet.of(P1M2_1)).build());
+		deliveries.add(new Delivery.Builder(F2P1_1, FARM_2, PRODUCER_1)
+				.connectedDeliveries(ImmutableSet.of(), ImmutableSet.of(P1M1_1)).build());
+		deliveries.add(new Delivery.Builder(F2P2_1, FARM_2, PRODUCER_2)
+				.connectedDeliveries(ImmutableSet.of(), ImmutableSet.of(P2M2_1)).build());
+		deliveries.add(new Delivery.Builder(F3P2_1, FARM_3, PRODUCER_2)
+				.connectedDeliveries(ImmutableSet.of(), ImmutableSet.of(P2M3_1)).build());
+
+		deliveries.add(new Delivery.Builder(P1M1_1, PRODUCER_1, MARKET_1)
+				.connectedDeliveries(ImmutableSet.of(F1P1_1, F2P1_1), ImmutableSet.of()).build());
+		deliveries.add(new Delivery.Builder(P1M2_1, PRODUCER_1, MARKET_2)
+				.connectedDeliveries(ImmutableSet.of(F1P1_2), ImmutableSet.of()).build());
+		deliveries.add(new Delivery.Builder(P2M2_1, PRODUCER_2, MARKET_2)
+				.connectedDeliveries(ImmutableSet.of(F2P2_1), ImmutableSet.of()).build());
+		deliveries.add(new Delivery.Builder(P2M3_1, PRODUCER_2, MARKET_3)
+				.connectedDeliveries(ImmutableSet.of(F3P2_1), ImmutableSet.of()).build());
 	}
 
 	@Test
-	public void testTraceWithCrossContamination() {
-		assertTrue(false);
-	}
+	public void testStationToStationTrace() {
+		Tracing tracing = new Tracing(deliveries);
+		Tracing.Result result = tracing.getResult(true);
 
-	@Test
-	public void testTraceWithMerging() {
-		assertTrue(false);
+		assertEquals(ImmutableSet.of(PRODUCER_1, MARKET_1, MARKET_2), result.getForwardStationsByStation().get(FARM_1));
+		assertEquals(ImmutableSet.of(PRODUCER_1, PRODUCER_2, MARKET_1, MARKET_2),
+				result.getForwardStationsByStation().get(FARM_2));
+		assertEquals(ImmutableSet.of(PRODUCER_2, MARKET_3), result.getForwardStationsByStation().get(FARM_3));
+		assertEquals(ImmutableSet.of(), result.getBackwardStationsByStation().get(FARM_1));
+		assertEquals(ImmutableSet.of(), result.getBackwardStationsByStation().get(FARM_2));
+		assertEquals(ImmutableSet.of(), result.getBackwardStationsByStation().get(FARM_3));
+
+		assertEquals(ImmutableSet.of(), result.getForwardStationsByStation().get(MARKET_1));
+		assertEquals(ImmutableSet.of(), result.getForwardStationsByStation().get(MARKET_2));
+		assertEquals(ImmutableSet.of(), result.getForwardStationsByStation().get(MARKET_3));
+		assertEquals(ImmutableSet.of(PRODUCER_1, FARM_1, FARM_2), result.getBackwardStationsByStation().get(MARKET_1));
+		assertEquals(ImmutableSet.of(PRODUCER_1, PRODUCER_2, FARM_1, FARM_2),
+				result.getBackwardStationsByStation().get(MARKET_2));
+		assertEquals(ImmutableSet.of(PRODUCER_2, FARM_3), result.getBackwardStationsByStation().get(MARKET_3));
 	}
 }
