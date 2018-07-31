68e7b44465399f323167dd4e53fcd3ecfab15e14
Marco Ruegen <marco.ruegen@bfr.bund.de>
2018-07-31 12:40:16 +0200

    Issue #404 fixed.

--- de.bund.bfr.knime.openkrise.db/src/de/bund/bfr/knime/openkrise/db/gui/simsearch/SimSearchJFrame.java
+++ de.bund.bfr.knime.openkrise.db/src/de/bund/bfr/knime/openkrise/db/gui/simsearch/SimSearchJFrame.java
@@ -890,60 +890,118 @@ public class SimSearchJFrame extends JDialog implements SimSearch.SimSearchListe
         //System.out.println("prepareSettingsChange leaving");
 	}
 
+	
 	private void processUserHelpRequest() {
-		String help = "<html>\n";
+      String help = "<html>\n";
 
-		help += "This view shows the findings of the similarity search.\n";
-		help += "The table lists either similar stations, products, lots or deliveries. One line of the result list shows the " + 
-				SimSearchJTable.RowHeaderColumnRenderer.HTML_SYMBOL_SIM_REFERENCE + " symbol which refers to the row which was used for the comparison and was found to be similar to all the other rows. ";
-		help += "<br>\n";
-		help += "Some of the columns may contain colored text or special symbols like " + 
-				SimSearchJTable.AlignmentColumnRenderer.getColoredMismatchText( SimSearchJTable.AlignmentColumnRenderer.SYMBOL_GAP) + ", " +
-				SimSearchJTable.AlignmentColumnRenderer.getColoredMismatchText(SimSearchJTable.AlignmentColumnRenderer.SYMBOL_SPACE_DELETE) + ", " + 
-				SimSearchJTable.AlignmentColumnRenderer.getColoredNeutralGap() + ".";
-		help += " The coloring reflects the difference of the text to the text in the comparison row (" +  
-		                    SimSearchJTable.RowHeaderColumnRenderer.HTML_SYMBOL_SIM_REFERENCE + ").";
+      help += "<h1>Similarity Search</h1>";
+      help += "A similarity search is used to get rid of duplicate entries in the database. It is recommended after importing data into the database. Otherwise, the food chain shown in the Tracing View graph might not reflect the true food chain and tracing might be less successful.";
+      help += "<br><br><h2>Background</h2>";
+      help += "An important prerequisite for tracing analysis is to have good data quality. Ideally, each individual station, product, lot and delivery is represented only once in the database. As FoodChain-Lab identifies stations (and also products, lots and deliveries) on the basis of their properties (name and address), a second import of the identical station but spelled slightly different would result in a new entry for this station. It would now be a duplicate and for a correct analysis duplicates should be merged to one entry using the similarity search.";
+      help +="<br>The algorithm used to find similarities is the Dice's similarity coefficient (see https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient).";
+      help += "<br><br>";
+      help += "<h2>Similarity search settings</h2>";
+      help += "Choose the category to be checked for similarity: Station, Product, Lot or Delivery. To change the percentage of similarity for each parameter either click into the white boxes and type the desired value in or use the up and down arrows. Parameters which are shown greyed out cannot be changed and are only displayed to inform on all the parameters which are considered in a similarity search.";
+      help += "For the category ‘Station’ two parameters can be adjusted (‘Name’ and ‘Address’). Both are connected via a logical AND meaning that only those stations will be shown as similar for which both similarity parameter values are true at the same time.";
+      help += "<br>";
+      help += "<h3>Examples:</h3>";
+      help += "<ul>";
+      help += "<li>50% similarity means that half of the letters of a name should be identical. Now, for every entry the search algorithm lists all other entries having 50% similarity.</li>";
+      help += "<li>To find stations with names that are at least 90% identical and at the same time have at least 80% similarity in their addresses enter ‘Name’ = 90 and ‘Address’ = 80.</li>";
+      help += "<li>To find stations with identical addresses the station names are irrelevant and thus ‘Name’ = 0% and ‘Address’ = 100% will result in stations (e.g. companies) which are located at the same address. This is helpful if several different data providers send information on identical companies but with varying names (e.g. ‘Luxury Cakes Limited’ and ‘Luxury Cakes Ltd.’, both with identical addresses).</li>";
+      help += "<li>A value of 100 means that two items are treated as 'similar' only if they are to 100% identical. Product names ‘Strawberry’ and ‘Strawberries’ would not be listed as similar in this case.</li>";
+      help += "<li>A value of 0 means that two items are always treated as 'similar'. With this setting, even the two product names ‘strawberry’ and ‘meat’ would be shown in the results table.</li>";
+      help += "</ul>";
+      help += "<br>";
+      help += "Check option 'Also show results previously marked as unique' to disable filtering of similarities which have already been decided on.";
+      help += "<br>";
+      help += "<br>Example:<br>";
+      help += "Two stations were displayed as being similar but as they were in reality different, the user clicked the button ‘All stations are unique’. After some time the user is not so sure anymore whether this decision had been correct. Using the same parameter settings for ‘Name’ and ‘Address’ as in the previous search these two stations will be displayed in the results table again if 'Also show results previously marked as unique' is checked.";
+      help += "<br><br>";
+      help += "<h2>Search Results</h2>";
+      help += "In the resulting table all differences are highlighted. Now the user must decide whether there are any duplicates (or whether all items are unique). Duplicates should be merged.";
+      help += "<br>";
+      help += "<br>Symbols:";
+      help += "<ul>";
+      help += "<li>‘" + SimSearchJTable.RowHeaderColumnRenderer.HTML_SYMBOL_SIM_REFERENCE + "’ in the first column indicates the comparison row. This entry was used for comparison with all other entries in the database. The comparison row is not to be understood as a preference.</li>";
+      help += "<li>‘" + SimSearchJTable.AlignmentColumnRenderer.SYMBOL_GAP + "’ represents a gap in a text. This gap is necessary to be able to align this text to another one.</li>";
+      help += "<li>‘" + SimSearchJTable.AlignmentColumnRenderer.getColoredMismatchText(SimSearchJTable.AlignmentColumnRenderer.SYMBOL_SPACE_DELETE) + "’ represents a space character in a text which is aligned to a different character or a gap in the comparison text.</li>";
+      help += "</ul>";
+      help += "<br>";
+      help += "Colours:<br>";
+      help += "The colouring reflects the difference of the text to the text in the comparison row:<br>";
+      help += "<ul>";
+      help += "<li>Black indicates no difference.</li>"; 
+      help += "<li>Red indicates a difference to the text in the comparison row.</li>"; 
+      help += "<li>Green indicates a gap in a text which is aligned to a character in some other text.</li>";
+      help += "</ul>";
+      help += "<br>";
 
-		help += "<br><h2>Color coding</h2>\n";
-		help += "The visualizations of the text comparisons use 3 colors.";
-		help += " The color red indicates a difference from a text to the text in the comparison row. Black indicates no difference. And green indicates a gap in a text which is aligned to a character in some other text.";
-		help += "The symbols " + SimSearchJTable.AlignmentColumnRenderer.getColoredNeutralGap() + " and " + 
-				SimSearchJTable.AlignmentColumnRenderer.getColoredMismatchText( SimSearchJTable.AlignmentColumnRenderer.SYMBOL_GAP) + " represent a gap in a text.";
+      help += "Example:";
+      Alignment.AlignedSequence[] alignedSeq = Alignment.alignSequences(new String[] {"Am Burggraben 128",  "Am Burg grabem 18", "Amm Bruggraben 128"},0);
+      try {
+          help += "<br><table cellpadding=0>" +
+                  "<tr><td>Address 1 in comparison row (" + SimSearchJTable.RowHeaderColumnRenderer.HTML_SYMBOL_SIM_REFERENCE + "):&nbsp;</td><td>" + 
+                  SimSearchJTable.AlignmentColumnRenderer.createHtmlCode(alignedSeq[0], false) + "</td></tr>" + 
+                  "<tr><td>Address 2 in row x:</td><td>" + SimSearchJTable.AlignmentColumnRenderer.createHtmlCode(alignedSeq[1], false) + "</td></tr>" + 
+                  "<tr><td>Address 3 in row y:</td><td>" + SimSearchJTable.AlignmentColumnRenderer.createHtmlCode(alignedSeq[2], false) + "</td></tr>" + 
+                  "</table><br>" ;
+      } catch (Exception e) {
+          help += "<i>Example is missing.</i>";
+      } 
 
-		help += "The symbol " + SimSearchJTable.AlignmentColumnRenderer.getColoredMismatchText(SimSearchJTable.AlignmentColumnRenderer.SYMBOL_SPACE_DELETE) +
-				" represents a space character in a text which is aligned to a different character or a gap in the comparison text."; 
+      help += "This example shows an alignment of three addresses. Address 3 has a double ‘mm’ which is different to the address in the comparison row. Address 2 has a space character between the double ‘gg’ which is not matched by the comparison sequence."; 
+      help += "<br><br>"; 
+      help += "Navigating through search results:<br>"; 
+      help += "Using the ‘&lt;’ or ‘>’ buttons the user can switch to the previous or next result. With the buttons ‘&lt;&lt;’ and ‘>>’ it is possible to jump to the first or last result."; 
+      help += "<br><br>";  
+      help += "Please note: '…' below a text within a cell indicates a multiline text. To see the complete text either use the mouse to display the tooltip of the cell, or increase the row size by dragging the bottom border of the first column."; 
+      
 
-		help += " The coloring is described in the following example:<br>";
-		Alignment.AlignedSequence[] alignedSeq = Alignment.alignSequences(new String[] {"Am Burggraben 128",  "Am Burg grabem 18", "Amm Bruggraben 128"},0);
-		try {
-			help += "<br><table cellpadding=0>" +
-					"<tr><td>Text 1 in comparison row (" + SimSearchJTable.RowHeaderColumnRenderer.HTML_SYMBOL_SIM_REFERENCE + "):&nbsp;</td><td>" + 
-					SimSearchJTable.AlignmentColumnRenderer.createHtmlCode(alignedSeq[0], false) + "</td></tr>" + 
-					"<tr><td>Text 2 in row x:</td><td>" + SimSearchJTable.AlignmentColumnRenderer.createHtmlCode(alignedSeq[1], false) + "</td></tr>" + 
-					"<tr><td>Text 3 in row y:</td><td>" + SimSearchJTable.AlignmentColumnRenderer.createHtmlCode(alignedSeq[2], false) + "</td></tr>" + 
-					"</table><br>" ;
-		} catch (Exception e) {
-			help += "<i>Example is missing.</i>";
-		} 
+      help += "<br><br>";
+      help += "<h2>Merging</h2>";
+      help += "Two rows which belong to the identical element (station, product, lot or delivery) shall be merged. This can be accomplished by a drag-and-drop action: Left click and hold the duplicate row and drop it onto the row that should be kept. It is like moving a file (or a selection of files) to a folder in a file browser. Any row can be merged into any other row (which has not been merged, yet).";
+      help += "<br>Merging can be undone by clicking the <b>undo</b> button above the table or by choosing <b>unmerge</b> in the context menu (right-click on a merged row)."; 
+      help += "<br><br>"; 
+      help += "Please note that dropping one or more rows <b>between</b> other rows does not result in a merge, but in a changed row order."; 
+      
+      help += "<br><br>";
+      help += "<h2>Marking as being unique</h2>";
+      help += "If only the comparison row is unique, click the button ‘<b>Comparison station/product/lot/deliveries is unique</b>’ to mark all rows (that are not already merged) as different to the comparison row.";
+      help += "<br>If all rows are unique click the button ‘<b>All stations/products/lots/deliveries are unique</b>’ to mark all pairs of rows (that are not already merged) as different to each other."; 
+      help += "<br>";
+      help += "<br>After pressing one of the two buttons the table automatically switches to the next result.";
+      help += "<br>";
+      help += "<br>Once marked as unique, those results are not shown again in later similarity searches within this database. In order to show ‘markings as being unique’ the option ‘<b>Also show results previously marked as unique</b>‘ in the search settings can be checked."; 
 
-		help += "This example shows an alignment of the 3 texts. Text 3 has a double &lsquo;mm&rsquo; which is different to the text in the comparison row. Text 2 has a space character between the double &lsquo;gg&rsquo; which is not matched by the comparison sequence.";
+      help += "<br>";
+      help += "<h3>Filtering</h3>";
+      help += "In the top left corner there is a filter panel. Items in the table can be filtered by entering a filter text. For a more sophisticated filtering check the option “use regular expressions”.";
 
-		help += "<h2>Merging rows</h2>\n";
-		help += "To merge stations/products/lots/deliveries you need only to drag the representing row(s) onto the station/product/lot/delivery you want them to be merged into. It is like moving a file (or a selection of files) to a folder in a file browser.<br>" +
-		"You can merge any row into any other row (which was not already merged). If e.g.  two rows are representing the same station/product/lot/delivery, you have to decide which row to merge onto which. The comparison row is not to be understood as a preference.<br>" +
-		    "Please note, that a drop of a row/rows between other rows does not result in a merge, but in another row ordering.";
+      help += "<br><br>";
+      help += "<h2>View Options</h2>";
+      help += "<ul margin-bottom=0>";
+        help += "<li>Rows which have been merged can be hidden in the results table by checking ‘<b>Hide merged rows</b>’ in the View menu.</li>";
+        help += "<li>Rows can be:";
+          help += "<ul margin-top=0 margin-bottom=0>";
+            help += "<li>Sorted: by left clicking on the column the rows shall be sorted with respect to it, a second click reverses the ordering</li>";
+            help += "<li>Resized: by dragging its bottom border in the first column</li>";
+          help += "</ul></li>";
+        help += "<li>The font size can be increased or decreased by clicking the respective option in <b>Table Font Size</b> in the ‘<b>View</b>’ menu or via pressing <b>Ctrl+Plus</b> or <b>Ctrl+Minus</b>.</li>"; 
+        help += "<li>Columns can be";
+          help += "<ul>";
+            help += "<li>Moved: Drag & drop on column headline with the left mouse button</li>";
+            help += "<li>Hidden/shown: Right mouse button on column headline, show columns, check column names</li>";
+            help += "<li>Frozen/unfrozen: Right mouse button on column headline, Freeze/Unfreeze. Frozen columns are moved to the left of the table. (Please note: There can be only one frozen column.)</li>";
+            help += "<li>Resized: By dragging the right side of its header</li>";
+          help += "</ul>";
+        help += "</li>";
+      help += "</ul>";
+      help += "</html>\n";
 
-		help += "<h2>Ignore rows</h2>\n";
-		help += "You can also mark similar rows as different to exclude them from future similarity searches. The button &lsquo;<strong>Comparison station/product/lot/deliveries is unique</strong>&rsquo; marks all rows (that are not already merged) as different to the comparison row.  The button &lsquo;<strong>All stations/products/lots/deliveries are unique</strong>&rsquo; marks all pairs of rows (that are not already merged) as different to each other.";
-		help += "The result of this action might be that the view switches to the next finding. ";
-		
-		help += "<h2>View customization</h2>\n";
-        help += "You can disable the showing of already merged rows, by checking the &lsquo;<strong>Hide merged rows</strong>&rsquo; entry in the &lsquo;<strong>View</strong>&rsquo; menu.";
-		
-		help += "</html>\n";
+      InfoBox ib = new InfoBox(this, help, true, new Dimension(800, 400), null, true, true);
+      ib.setVisible(true);
+  }
 
-		InfoBox ib = new InfoBox(this, help, true, new Dimension(800, 400), null, true, true);
-		ib.setVisible(true);
-	}
 
 }
