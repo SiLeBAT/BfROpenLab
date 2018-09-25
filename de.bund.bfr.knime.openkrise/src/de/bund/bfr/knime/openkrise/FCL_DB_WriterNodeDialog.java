0736ce2cb0367d3bd8704a6c5f9cf86eeb8f7691
draaw <armin.weiser@bfr.bund.de>
2018-09-25 18:19:43 +0200

    FCL_DB_Writer updated - now Columns to be saved can be individually defined

--- de.bund.bfr.knime.openkrise/src/de/bund/bfr/knime/openkrise/FCL_DB_WriterNodeDialog.java
+++ de.bund.bfr.knime.openkrise/src/de/bund/bfr/knime/openkrise/FCL_DB_WriterNodeDialog.java
@@ -19,10 +19,18 @@
  *******************************************************************************/
 package de.bund.bfr.knime.openkrise;
 
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import org.knime.core.data.DataTableSpec;
 import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeDialogPane;
 import org.knime.core.node.NodeSettingsRO;
@@ -41,14 +49,107 @@ public class FCL_DB_WriterNodeDialog extends NodeDialogPane {
 	private FCL_DB_WriterSettings set;
 
 	private JCheckBox clearDB;
-
+	DataTableSpec nodeSpec = null;
+	DataTableSpec edgeSpec = null;
+	DataTableSpec tracingSpec = null;
+	JComboBox<String> dbSId = new JComboBox<>();
+	JComboBox<String> dbSName = new JComboBox<>();
+	JComboBox<String> dbSAddress = new JComboBox<>();
+	JComboBox<String> dbSCountry = new JComboBox<>();
+	JComboBox<String> dbSTOB = new JComboBox<>();
+	JComboBox<String>[] sCombos = new JComboBox[]{dbSId, dbSName, dbSAddress, dbSCountry, dbSTOB};
+	
+	JComboBox<String> dbDId = new JComboBox<>();
+	JComboBox<String> dbDFrom = new JComboBox<>();
+	JComboBox<String> dbDTo = new JComboBox<>();
+	JComboBox<String> dbDName = new JComboBox<>();
+	JComboBox<String> dbDEAN = new JComboBox<>();
+	JComboBox<String> dbDLot = new JComboBox<>();
+	JComboBox<String> dbDBestBefore = new JComboBox<>();
+	JComboBox<String> dbDDDD = new JComboBox<>();
+	JComboBox<String> dbDDDM = new JComboBox<>();
+	JComboBox<String> dbDDDY = new JComboBox<>();
+	JComboBox<String> dbDAmount = new JComboBox<>();
+	JComboBox<String> dbDComment = new JComboBox<>();
+	JComboBox<String>[] dCombos = new JComboBox[]{dbDId, dbDFrom, dbDTo, dbDName, dbDEAN, dbDLot, dbDBestBefore, dbDDDD, dbDDDM, dbDDDY, dbDAmount, dbDComment};
+	
+	JComboBox<String> dbTFrom = new JComboBox<>();
+	JComboBox<String> dbTTo = new JComboBox<>();
+	JComboBox<String>[] tCombos = new JComboBox[]{dbTFrom, dbTTo};
 	protected FCL_DB_WriterNodeDialog() {
 		JPanel dbPanel = new JPanel();
 
 		clearDB = new JCheckBox("Clear DB before writing");
 		dbPanel.setLayout(new BoxLayout(dbPanel, BoxLayout.Y_AXIS));
-		dbPanel.add(UI.createWestPanel(UI.createBorderPanel(clearDB)));
+		dbPanel.add(UI.createNorthPanel(UI.createBorderPanel(clearDB)));
+		
+	    JPanel sPanel = new JPanel();
+	    sPanel.setLayout(new GridBagLayout());
+	    GridBagConstraints c = new GridBagConstraints();
+	    c.fill = GridBagConstraints.HORIZONTAL;
+		
+	    int y = 0;
+	    JLabel label = new JLabel("Stations", JLabel.HORIZONTAL);
+	    label.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+	    c.gridx = 0; c.gridy = y; c.gridwidth = 2;
+	    sPanel.add(label, c);
+	    
+	    c.fill = GridBagConstraints.HORIZONTAL; c.gridwidth = 1;
+	    y++; c.gridx = 0; c.gridy = y; sPanel.add(new JLabel("Id: ", JLabel.RIGHT), c);		
+	    c.gridx = 1; c.gridy = y; sPanel.add(dbSId, c);		
+	    y++; c.gridx = 0; c.gridy = y; sPanel.add(new JLabel("Name: ", JLabel.RIGHT), c);		
+	    c.gridx = 1; c.gridy = y; sPanel.add(dbSName, c);		
+	    y++; c.gridx = 0; c.gridy = y; sPanel.add(new JLabel("Address: ", JLabel.RIGHT), c);		
+	    c.gridx = 1; c.gridy = y; sPanel.add(dbSAddress, c);		
+	    y++; c.gridx = 0; c.gridy = y; sPanel.add(new JLabel("Country: ", JLabel.RIGHT), c);		
+	    c.gridx = 1; c.gridy = y; sPanel.add(dbSCountry, c);		
+	    y++; c.gridx = 0; c.gridy = y; sPanel.add(new JLabel("Type Of Business: ", JLabel.RIGHT), c);		
+	    c.gridx = 1; c.gridy = y; sPanel.add(dbSTOB, c);		
+	    
+	    label = new JLabel("Deliveries", JLabel.HORIZONTAL);
+	    label.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+	    y++; c.gridx = 0; c.gridy = y; c.gridwidth = 2;
+	    sPanel.add(label, c);
 
+	    c.fill = GridBagConstraints.HORIZONTAL; c.gridwidth = 1;
+	    y++; c.gridx = 0; c.gridy = y; sPanel.add(new JLabel("Id: ", JLabel.RIGHT), c);		
+	    c.gridx = 1; c.gridy = y; sPanel.add(dbDId, c);		
+	    y++; c.gridx = 0; c.gridy = y; sPanel.add(new JLabel("from: ", JLabel.RIGHT), c);		
+	    c.gridx = 1; c.gridy = y; sPanel.add(dbDFrom, c);		
+	    y++; c.gridx = 0; c.gridy = y; sPanel.add(new JLabel("to: ", JLabel.RIGHT), c);		
+	    c.gridx = 1; c.gridy = y; sPanel.add(dbDTo, c);		
+	    y++; c.gridx = 0; c.gridy = y; sPanel.add(new JLabel("Name: ", JLabel.RIGHT), c);		
+	    c.gridx = 1; c.gridy = y; sPanel.add(dbDName, c);		
+	    y++; c.gridx = 0; c.gridy = y; sPanel.add(new JLabel("EAN: ", JLabel.RIGHT), c);		
+	    c.gridx = 1; c.gridy = y; sPanel.add(dbDEAN, c);		
+	    y++; c.gridx = 0; c.gridy = y; sPanel.add(new JLabel("Lot Number: ", JLabel.RIGHT), c);		
+	    c.gridx = 1; c.gridy = y; sPanel.add(dbDLot, c);		
+	    y++; c.gridx = 0; c.gridy = y; sPanel.add(new JLabel("Best Before Date: ", JLabel.RIGHT), c);		
+	    c.gridx = 1; c.gridy = y; sPanel.add(dbDBestBefore, c);		
+	    y++; c.gridx = 0; c.gridy = y; sPanel.add(new JLabel("Date Delivery - Day: ", JLabel.RIGHT), c);		
+	    c.gridx = 1; c.gridy = y; sPanel.add(dbDDDD, c);		
+	    y++; c.gridx = 0; c.gridy = y; sPanel.add(new JLabel("Date Delivery - Month: ", JLabel.RIGHT), c);		
+	    c.gridx = 1; c.gridy = y; sPanel.add(dbDDDM, c);		
+	    y++; c.gridx = 0; c.gridy = y; sPanel.add(new JLabel("Date Delivery - Year: ", JLabel.RIGHT), c);		
+	    c.gridx = 1; c.gridy = y; sPanel.add(dbDDDY, c);		
+	    y++; c.gridx = 0; c.gridy = y; sPanel.add(new JLabel("Amount: ", JLabel.RIGHT), c);		
+	    c.gridx = 1; c.gridy = y; sPanel.add(dbDAmount, c);		
+	    y++; c.gridx = 0; c.gridy = y; sPanel.add(new JLabel("Comment: ", JLabel.RIGHT), c);		
+	    c.gridx = 1; c.gridy = y; sPanel.add(dbDComment, c);		
+
+	    
+	    label = new JLabel("Tracing - Relations", JLabel.HORIZONTAL);
+	    label.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+	    y++; c.gridx = 0; c.gridy = y; c.gridwidth = 2;
+	    sPanel.add(label, c);
+
+	    c.fill = GridBagConstraints.HORIZONTAL; c.gridwidth = 1;
+	    y++; c.gridx = 0; c.gridy = y; sPanel.add(new JLabel("from: ", JLabel.RIGHT), c);		
+	    c.gridx = 1; c.gridy = y; sPanel.add(dbTFrom, c);		
+	    y++; c.gridx = 0; c.gridy = y; sPanel.add(new JLabel("to: ", JLabel.RIGHT), c);		
+	    c.gridx = 1; c.gridy = y; sPanel.add(dbTTo, c);		
+
+	    dbPanel.add(sPanel);
 		addTab("Database", UI.createNorthPanel(dbPanel));
 
 		set = new FCL_DB_WriterSettings();
@@ -58,11 +159,77 @@ public class FCL_DB_WriterNodeDialog extends NodeDialogPane {
 	protected void loadSettingsFrom(final NodeSettingsRO settings, final PortObjectSpec[] specs) {
 		set.loadSettings(settings);
 		clearDB.setSelected(set.isClearDB());
+
+		nodeSpec = (DataTableSpec) specs[0];
+		edgeSpec = (DataTableSpec) specs[1];
+		tracingSpec = (DataTableSpec) specs[2];
+		if (nodeSpec != null && edgeSpec != null && tracingSpec != null) {
+			for (JComboBox<String> combo : sCombos) {
+				combo.removeAllItems();
+				combo.addItem("");
+				for (String col : nodeSpec.getColumnNames()) combo.addItem(col);
+			}
+			for (JComboBox<String> combo : dCombos) {
+				combo.removeAllItems();
+				combo.addItem("");
+				for (String col : edgeSpec.getColumnNames()) combo.addItem(col);
+			}
+			for (JComboBox<String> combo : tCombos) {
+				combo.removeAllItems();
+				combo.addItem("");
+				for (String col : tracingSpec.getColumnNames()) combo.addItem(col);
+			}
+		}
+		
+		dbSId.setSelectedItem(set.getDbSId());
+		dbSName.setSelectedItem(set.getDbSName());
+		dbSAddress.setSelectedItem(set.getDbSAddress());
+		dbSCountry.setSelectedItem(set.getDbSCountry());
+		dbSTOB.setSelectedItem(set.getDbSTOB());
+
+		dbDId.setSelectedItem(set.getDbDId());
+		dbDName.setSelectedItem(set.getDbDName());
+		dbDFrom.setSelectedItem(set.getDbDFrom());
+		dbDTo.setSelectedItem(set.getDbDTo());
+		dbDEAN.setSelectedItem(set.getDbDEAN());
+		dbDLot.setSelectedItem(set.getDbDLot());
+		dbDBestBefore.setSelectedItem(set.getDbDBestBefore());
+		dbDDDD.setSelectedItem(set.getDbDDDD());
+		dbDDDM.setSelectedItem(set.getDbDDDM());
+		dbDDDY.setSelectedItem(set.getDbDDDY());
+		dbDAmount.setSelectedItem(set.getDbDAmount());
+		dbDComment.setSelectedItem(set.getDbDComment());
+		
+		dbTFrom.setSelectedItem(set.getDbTFrom());
+		dbTTo.setSelectedItem(set.getDbTTo());
 	}
 
 	@Override
 	protected void saveSettingsTo(final NodeSettingsWO settings) throws InvalidSettingsException {
 		set.setClearDB(clearDB.isSelected());
+		
+		set.setDbSId((String) dbSId.getSelectedItem());
+		set.setDbSName((String) dbSName.getSelectedItem());
+		set.setDbSAddress((String) dbSAddress.getSelectedItem());
+		set.setDbSCountry((String) dbSCountry.getSelectedItem());
+		set.setDbSTOB((String) dbSTOB.getSelectedItem());
+		
+		set.setDbDId((String) dbDId.getSelectedItem());
+		set.setDbDName((String) dbDName.getSelectedItem());
+		set.setDbDFrom((String) dbDFrom.getSelectedItem());
+		set.setDbDTo((String) dbDTo.getSelectedItem());
+		set.setDbDEAN((String) dbDEAN.getSelectedItem());
+		set.setDbDLot((String) dbDLot.getSelectedItem());
+		set.setDbDBestBefore((String) dbDBestBefore.getSelectedItem());
+		set.setDbDDDD((String) dbDDDD.getSelectedItem());
+		set.setDbDDDM((String) dbDDDM.getSelectedItem());
+		set.setDbDDDY((String) dbDDDY.getSelectedItem());
+		set.setDbDAmount((String) dbDAmount.getSelectedItem());
+		set.setDbDComment((String) dbDComment.getSelectedItem());
+				
+		set.setDbTFrom((String) dbTFrom.getSelectedItem());
+		set.setDbTTo((String) dbTTo.getSelectedItem());
+
 		set.saveSettings(settings);
 	}
 }
--- de.bund.bfr.knime.openkrise/src/de/bund/bfr/knime/openkrise/FCL_DB_WriterSettings.java
+++ de.bund.bfr.knime.openkrise/src/de/bund/bfr/knime/openkrise/FCL_DB_WriterSettings.java
@@ -28,8 +28,52 @@ import de.bund.bfr.knime.NodeSettings;
 public class FCL_DB_WriterSettings extends NodeSettings {
 
 	private static final String CFG_CLEAR_DB = "ClearDB";
+	
+	private static final String CFG_SID_DB = "SID";
+	private static final String CFG_SNAME_DB = "SNAME";
+	private static final String CFG_SADDRESS_DB = "SADDRESS";
+	private static final String CFG_SCOUNTRY_DB = "SCOUNTRY";
+	private static final String CFG_STOB_DB = "STOB";
+	
+	private static final String CFG_DID_DB = "DID";
+	private static final String CFG_DNAME_DB = "DNAME";
+	private static final String CFG_DEAN_DB = "DEAN";
+	private static final String CFG_DFROM_DB = "DFROM";
+	private static final String CFG_DTO_DB = "DTO";
+	private static final String CFG_DLOT_DB = "DLOT";
+	private static final String CFG_DBESTBEFORE_DB = "DBESTBEFORE";
+	private static final String CFG_DDDD_DB = "DDDD";
+	private static final String CFG_DDDM_DB = "DDDM";
+	private static final String CFG_DDDY_DB = "DDDY";
+	private static final String CFG_DAMOUNT_DB = "DAMOUNT";
+	private static final String CFG_DCOMMENT_DB = "DCOMMENT";
+	
+	private static final String CFG_TFROM_DB = "TFROM";
+	private static final String CFG_TTO_DB = "TTO";
 
 	private boolean clearDB;
+	
+	private String dbSId = "ID";
+	private String dbSName = "Name";
+	private String dbSAddress = "Address";
+	private String dbSCountry = "Country";
+	private String dbSTOB = "type of business";
+	
+	private String dbDId = "ID";
+	private String dbDFrom = "from";
+	private String dbDTo = "to";
+	private String dbDName = "Name";
+	private String dbDEAN = "EAN";
+	private String dbDLot = "Lot Number";
+	private String dbDBestBefore = "BestBefore";
+	private String dbDDDD = "Date Delivery - Day";
+	private String dbDDDM = "Date Delivery - Month";
+	private String dbDDDY = "Date Delivery - Year";
+	private String dbDAmount = "Amount";
+	private String dbDComment = "Comment";
+
+	private String dbTFrom = "from";
+	private String dbTTo = "to";
 
 	public FCL_DB_WriterSettings() {
 		clearDB = false;
@@ -39,13 +83,94 @@ public class FCL_DB_WriterSettings extends NodeSettings {
 	public void loadSettings(NodeSettingsRO settings) {
 		try {
 			clearDB = settings.getBoolean(CFG_CLEAR_DB);
-		} catch (InvalidSettingsException e) {
-		}
+		} catch (InvalidSettingsException e) {}
+		
+		try {
+			dbSId = settings.getString(CFG_SID_DB);
+		} catch (InvalidSettingsException e) {}
+		try {
+			dbSName = settings.getString(CFG_SNAME_DB);
+		} catch (InvalidSettingsException e) {}
+		try {
+			dbSAddress = settings.getString(CFG_SADDRESS_DB);
+		} catch (InvalidSettingsException e) {}
+		try {
+			dbSCountry = settings.getString(CFG_SCOUNTRY_DB);
+		} catch (InvalidSettingsException e) {}
+		try {
+			dbSTOB = settings.getString(CFG_STOB_DB);
+		} catch (InvalidSettingsException e) {}
+		
+		try {
+			dbDId = settings.getString(CFG_DID_DB);
+		} catch (InvalidSettingsException e) {}
+		try {
+			dbDFrom = settings.getString(CFG_DFROM_DB);
+		} catch (InvalidSettingsException e) {}
+		try {
+			dbDTo = settings.getString(CFG_DTO_DB);
+		} catch (InvalidSettingsException e) {}
+		try {
+			dbDName = settings.getString(CFG_DNAME_DB);
+		} catch (InvalidSettingsException e) {}
+		try {
+			dbDEAN = settings.getString(CFG_DEAN_DB);
+		} catch (InvalidSettingsException e) {}
+		try {
+			dbDLot = settings.getString(CFG_DLOT_DB);
+		} catch (InvalidSettingsException e) {}
+		try {
+			dbDBestBefore = settings.getString(CFG_DBESTBEFORE_DB);
+		} catch (InvalidSettingsException e) {}
+		try {
+			dbDDDD = settings.getString(CFG_DDDD_DB);
+		} catch (InvalidSettingsException e) {}
+		try {
+			dbDDDM = settings.getString(CFG_DDDM_DB);
+		} catch (InvalidSettingsException e) {}
+		try {
+			dbDDDY = settings.getString(CFG_DDDY_DB);
+		} catch (InvalidSettingsException e) {}
+		try {
+			dbDAmount = settings.getString(CFG_DAMOUNT_DB);
+		} catch (InvalidSettingsException e) {}
+		try {
+			dbDComment = settings.getString(CFG_DCOMMENT_DB);
+		} catch (InvalidSettingsException e) {}
+		
+		try {
+			dbTFrom = settings.getString(CFG_TFROM_DB);
+		} catch (InvalidSettingsException e) {}
+		try {
+			dbTTo = settings.getString(CFG_TTO_DB);
+		} catch (InvalidSettingsException e) {}
 	}
 
 	@Override
 	public void saveSettings(NodeSettingsWO settings) {
 		settings.addBoolean(CFG_CLEAR_DB, clearDB);
+		
+		settings.addString(CFG_SID_DB, dbSId);
+		settings.addString(CFG_SNAME_DB, dbSName);
+		settings.addString(CFG_SADDRESS_DB, dbSAddress);
+		settings.addString(CFG_SCOUNTRY_DB, dbSCountry);
+		settings.addString(CFG_STOB_DB, dbSTOB);
+		
+		settings.addString(CFG_DID_DB, dbDId);
+		settings.addString(CFG_DFROM_DB, dbDFrom);
+		settings.addString(CFG_DTO_DB, dbDTo);
+		settings.addString(CFG_DNAME_DB, dbDName);
+		settings.addString(CFG_DEAN_DB, dbDEAN);
+		settings.addString(CFG_DLOT_DB, dbDLot);
+		settings.addString(CFG_DBESTBEFORE_DB, dbDBestBefore);
+		settings.addString(CFG_DDDD_DB, dbDDDD);
+		settings.addString(CFG_DDDM_DB, dbDDDM);
+		settings.addString(CFG_DDDY_DB, dbDDDY);
+		settings.addString(CFG_DAMOUNT_DB, dbDAmount);
+		settings.addString(CFG_DCOMMENT_DB, dbDComment);
+		
+		settings.addString(CFG_TFROM_DB, dbTFrom);
+		settings.addString(CFG_TTO_DB, dbTTo);
 	}
 
 	public boolean isClearDB() {
@@ -54,6 +179,158 @@ public class FCL_DB_WriterSettings extends NodeSettings {
 
 	public void setClearDB(boolean clearDB) {
 		this.clearDB = clearDB;
+	}
+
+	public String getDbSId() {
+		return dbSId;
+	}
+
+	public void setDbSId(String dbSId) {
+		this.dbSId = dbSId;
+	}
+
+	public String getDbSName() {
+		return dbSName;
+	}
+
+	public void setDbSName(String dbSName) {
+		this.dbSName = dbSName;
+	}
+
+	public String getDbSAddress() {
+		return dbSAddress;
+	}
+
+	public void setDbSAddress(String dbSAddress) {
+		this.dbSAddress = dbSAddress;
+	}
+
+	public String getDbSCountry() {
+		return dbSCountry;
+	}
+
+	public void setDbSCountry(String dbSCountry) {
+		this.dbSCountry = dbSCountry;
+	}
+
+	public String getDbSTOB() {
+		return dbSTOB;
+	}
+
+	public void setDbSTOB(String dbSTOB) {
+		this.dbSTOB = dbSTOB;
+	}
+
+	public String getDbDId() {
+		return dbDId;
+	}
+
+	public void setDbDId(String dbDId) {
+		this.dbDId = dbDId;
+	}
+
+	public String getDbDFrom() {
+		return dbDFrom;
+	}
+
+	public void setDbDFrom(String dbDFrom) {
+		this.dbDFrom = dbDFrom;
+	}
+
+	public String getDbDTo() {
+		return dbDTo;
+	}
+
+	public void setDbDTo(String dbDTo) {
+		this.dbDTo = dbDTo;
+	}
+
+	public String getDbDName() {
+		return dbDName;
+	}
+
+	public void setDbDName(String dbDName) {
+		this.dbDName = dbDName;
+	}
+
+	public String getDbDEAN() {
+		return dbDEAN;
+	}
+
+	public void setDbDEAN(String dbDEAN) {
+		this.dbDEAN = dbDEAN;
+	}
+
+	public String getDbDLot() {
+		return dbDLot;
+	}
+
+	public void setDbDLot(String dbDLot) {
+		this.dbDLot = dbDLot;
+	}
+
+	public String getDbDBestBefore() {
+		return dbDBestBefore;
+	}
+
+	public void setDbDBestBefore(String dbDBestBefore) {
+		this.dbDBestBefore = dbDBestBefore;
+	}
+
+	public String getDbDDDD() {
+		return dbDDDD;
+	}
+
+	public void setDbDDDD(String dbDDDD) {
+		this.dbDDDD = dbDDDD;
+	}
+
+	public String getDbDDDM() {
+		return dbDDDM;
+	}
+
+	public void setDbDDDM(String dbDDDM) {
+		this.dbDDDM = dbDDDM;
+	}
+
+	public String getDbDDDY() {
+		return dbDDDY;
+	}
+
+	public void setDbDDDY(String dbDDDY) {
+		this.dbDDDY = dbDDDY;
+	}
+
+	public String getDbDAmount() {
+		return dbDAmount;
+	}
+
+	public void setDbDAmount(String dbDAmount) {
+		this.dbDAmount = dbDAmount;
+	}
+
+	public String getDbDComment() {
+		return dbDComment;
+	}
+
+	public void setDbDComment(String dbDComment) {
+		this.dbDComment = dbDComment;
+	}
+
+	public String getDbTTo() {
+		return dbTTo;
+	}
+
+	public void setDbTTo(String dbTTo) {
+		this.dbTTo = dbTTo;
+	}
+
+	public String getDbTFrom() {
+		return dbTFrom;
+	}
+
+	public void setDbTFrom(String dbTFrom) {
+		this.dbTFrom = dbTFrom;
 	}
 
 }
