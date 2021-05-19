20983558727bebb12112dfb3eefa09678403ceb2
Marco Ruegen <marco.ruegen@bfr.bund.de>
2021-05-20 00:30:17 +0200

    updates copyrights
    ticket: #471

--- de.bund.bfr.knime.openkrise.db/src/de/bund/bfr/knime/openkrise/db/gui/dbtable/sorter/MyStringSorter.java
+++ de.bund.bfr.knime.openkrise.db/src/de/bund/bfr/knime/openkrise/db/gui/dbtable/sorter/MyStringSorter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 German Federal Institute for Risk Assessment (BfR)
+ * Copyright (c) 2021 German Federal Institute for Risk Assessment (BfR)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
