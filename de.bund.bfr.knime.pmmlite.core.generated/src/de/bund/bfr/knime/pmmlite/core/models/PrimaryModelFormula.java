a65555ab8b1ffa46b80a5351c42151906003ba1f
Marco Ruegen <marco.ruegen@bfr.bund.de>
2022-06-10 12:52:43 +0200

    sets Copyrights to 2014-2022

--- de.bund.bfr.knime.pmmlite.core.generated/src/de/bund/bfr/knime/pmmlite/core/models/PrimaryModelFormula.java
+++ de.bund.bfr.knime.pmmlite.core.generated/src/de/bund/bfr/knime/pmmlite/core/models/PrimaryModelFormula.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021 German Federal Institute for Risk Assessment (BfR)
+ * Copyright (c) 2014-2022 German Federal Institute for Risk Assessment (BfR)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
